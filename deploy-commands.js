204d91d678d91de3dc4791348b56047458827ebb
ColonialDustyBee <jv192000@gmail.com>
2025-04-09 23:29:40 -0700

    Fixed error with dupe

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,50 +1,46 @@
-const {REST, Routes} = require('discord.js');
-const {clientId, token} = require('./config.json');
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const commands = []
-// Grab all command folders from command directories
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders){
-    // Grab all command files from command directories
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles){
-        const filePath = path.join(commandsPath, file);
-        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('js'));
-        // Grab slashcommandbuildertoJson Output of each command's data for deployment, allows us to edit commands without redeployment
-        for (const file of commandFiles){
-            const filePath = path.join(commandsPath, file);
-            const command = require(filePath);
-            if('data' in command && 'execute' in command){
-                commands.push(command.data.toJSON());
-            }
-            else{
-                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" proerty.`);
-            }
-        }
-    }
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
-// constructs and prepares a REST instance
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
 
-const rest = new REST().setToken(token); // Grabs discord token
-
-// Deploys commands
+// and deploy your commands!
 (async () => {
-    try{
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
-        // Refreshes all commands in guild with current set.
-        const data = await rest.put(
-            Routes.applicationCommands(clientId),
-            {body: [commands]},
-        );
-        console.log(`Successfully reloaded ${data.lengtnoh} application (/) commands.`);
-    }
-    catch(error){
-        console.error(error);
-    }
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
