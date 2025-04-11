ac545cfa0dad2dd300593cdb7f7461769555e51e
ColonialDustyBee <jv192000@gmail.com>
2025-04-11 01:03:12 -0700

    Incorporated cookies for efficient download

--- downloadedVideos/processLink.py
+++ downloadedVideos/processLink.py
@@ -1,10 +1,16 @@
 # All hail yt-dlp
 from yt_dlp import YoutubeDL 
 # Usually, yt-dlp will spawn a small identifiable string which can be used to filter videos for bot to pull from.
-URL = ['https://www.youtube.com/watch?v=KfS2eQXMDrM'] # If all is said and done, this should spawn as a child process
+# Must add cookies of some kind
+URL = ['https://www.youtube.com/watch?v=TBxS0XhdfmU'] # If all is said and done, this should spawn as a child process
 ydl_opts = {
-    'format': 'ext',  # MP4 video + M4A audio (fallback to best MP4)
+    'format': 'bestvideo[res=720p][ext=mp4]+bestaudio[acodec^=mp4a][ext=m4a]/best[ext=mp4]',
+    'merge_output_format': 'mp4',
+    'cookies': 'cookiesyoutubeprivate.txt',
+    'postprocessors': [{
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4',
+    }],
 }
-
-with YoutubeDL() as ydl:
+with YoutubeDL(ydl_opts) as ydl:
     ydl.download(URL)
