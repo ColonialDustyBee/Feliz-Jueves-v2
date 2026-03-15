49de22eb319d1c46908f44df3d7f71c30ba8adc4
ColonialDustyBee <jv192000@gmail.com>
2026-03-15 00:02:40 -0700

    Incorporated GCP VM start functionality

--- Buttons/Start.js
+++ Buttons/Start.js
@@ -1,16 +1,17 @@
 const compute = require('@google-cloud/compute');
 const path = require('node:path');
-const config = require(path.join(__dirname, '..', './config.gcp.json')); // The filename has to be called config.gcp.json, you could edit it to find your own name
+const config = require(path.join(__dirname, '..', 'config.gcp.json')); // The filename has to be called config.gcp.json, you could edit it to find your own name4
+const GCP_CONFIG = {
+    project: config.project,
+    zone: config.zone,
+    instance: config.instance
+}
 module.exports = { // Start
     async execute(interaction) {
         console.log("Checking if Minecraft Server can be ran")
         const instancesClient = new compute.InstancesClient();
         console.log('Acquiring vm status');
-        const [instance] = await instancesClient.get({
-            project: config.project,
-            zone: config.zone,
-            instance: config.instance
-        });
+        const [instance] = await instancesClient.get({GCP_CONFIG});
         try{
             if (instance.status === 'RUNNING' || instance.status === 'STAGING' || instance.status === 'RUNNING') { // Checks if the vm is already turned on
                 console.log('Minecraft server is already started');
@@ -21,18 +22,14 @@ module.exports = { // Start
             }
             else{
                 console.log("Starting minecraft server");
-                const [operation] = await instancesClient.start(GCP_CONFIG);
-                await operation.promise(); // Await the operation babey
-                console.log('Minecraft Server started!');
+                const [operation] = await instancesClient.start({GCP_CONFIG});
                 await interaction.editReply({
                     content: 'Minecraft server has started!',
                     components: []
                 });
+                console.log('Minecraft Server started!');
+                await operation.promise(); // Await the operation babey
             }
-            await interaction.editReply({
-                content: 'Started!',
-                components: [] // You have to declare the components as blank otherwise the buttons will not vanish. JavaScript in its grand wisdom ladies and gents
-            });
         }
         catch(error){
             await interaction.editReply("Something went wrong with starting the server");
